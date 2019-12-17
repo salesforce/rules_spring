a84af6b47eaadf04b4bf0c554afcaf1f23861af8
Peter <plaird@salesforce.com>
2019-12-17 10:59:12 -0700

    adding arg printout to sample app

--- samples/helloworld/src/main/java/com/sample/SampleMain.java
+++ samples/helloworld/src/main/java/com/sample/SampleMain.java
@@ -13,6 +13,14 @@ import org.springframework.boot.SpringApplication;
 public class SampleMain {
 
   public static void main(String[] args) {
+    System.out.println("Launching the sample SpringBoot demo application...");
+    StringBuffer sb = new StringBuffer();
+    for (String arg : args) {
+      sb.append(arg);
+      sb.append(" ");
+    }
+    System.out.println("  Command line args: "+sb.toString());
+
     SpringApplication.run(SampleMain.class, args);
   }
 

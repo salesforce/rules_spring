e4218b193218f3f4942e3ee33e88512a9944315f
Peter Laird <plaird@salesforce.com>
2020-12-10 18:26:45 -0700

    update makerelease.sh script

--- makerelease.sh
+++ makerelease.sh
@@ -1,3 +1,11 @@
 rm -rf bazel-*
 
-jar -cvf bazel-springboot-rule-1.0.1.zip *
+TMPDIR=/tmp/bazel-springboot-rule-release
+mkdir $TMPDIR
+mv tools/python_interpreter/bin $TMPDIR
+mv tools/python_interpreter/captive_python3 $TMPDIR
+
+jar -cvf bazel-springboot-rule-1.0.7.zip *
+
+mv $TMPDIR/bin tools/python_interpreter
+mv $TMPDIR/captive_python3 tools/python_interpreter

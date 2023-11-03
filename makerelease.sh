77763b7616ee60e4671b1751f0968566be3047cc
Peter Laird <plaird@salesforce.com>
2023-11-02 20:56:55 -0600

    more release refinements

--- makerelease.sh
+++ makerelease.sh
@@ -7,32 +7,55 @@
 
 release_version=2.2.5
 
+echo "RELEASE: You are about to create a local zip file that contains the release bits."
+echo "After it is created, there will be instructions on how to upload it to GitHub and create the release metadata."
+echo ""
+echo "Please enter the release version (e.g. 2.2.5):"
+read release_version
+echo ""
+echo "You have entered $release_version, are you good with this version? Enter to contine, ctrl-c to abort."
+read DOTHETHING
+
+# write the MODULE.bazel file
+echo "module(" > MODULE.bazel
+echo "  name = \"rules_spring\"," >> MODULE.bazel
+echo "  version = \"$release_version\"," >> MODULE.bazel
+echo "  repo_name = \"rules_spring\"," >> MODULE.bazel
+echo "  compatibility_level = 1," >> MODULE.bazel
+echo ")" >> MODULE.bazel
+
 # remove/relocate local build artifacts and tools
 rm -rf bazel-*
 
+# move some dev directories out, so they don't get zipped up in the archive
 tmpdir=/tmp/rules-spring-release
-mkdir $tmpdir
-mv tools/python_interpreter/bin $tmpdir
-mv tools/python_interpreter/captive_python3 $tmpdir
+mkdir $tmpdir 2>/dev/null
+mv tools/python_interpreter/bin $tmpdir 2>/dev/null
+mv tools/python_interpreter/captive_python3 $tmpdir 2>/dev/null
 
 rm -rf coverage-reports
 rm -rf springboot/tests/__pycache__
 rm -rf springboot/__pycache__
 
 # jar up the code
-jar -cvf rules-spring-${release_version}.zip *
+release_zip=rules-spring-${release_version}.zip
+jar -cvf $release_zip *
 
-# restore some of the local build tools
-mv $tmpdir/bin tools/python_interpreter
-mv $tmpdir/captive_python3 tools/python_interpreter
+# restore the dev directories
+mv $tmpdir/bin tools/python_interpreter 2>/dev/null
+mv $tmpdir/captive_python3 tools/python_interpreter 2>/dev/null
 
+rm -rf /tmp/rules-spring-release
+
+sha256=$(shasum -a 256 $release_zip)
+
+echo ""
 echo "RELEASE artifact built successfully: rules-spring-${release_version}.zip"
+echo "  SHA256 of the release artifact: $sha256"
 echo ""
 echo "Remember to complete these tasks to make the official release:"
 echo " 1. Create a new release on GitHub and upload the zip file to it. Look at previous releases and use the same doc conventions. Make sure you tag the release."
-echo " 2. Compute the SHA256 for the zip file, for example: shasum -a 256 rules_spring-2.2.5.zip"
-echo " 3. Update the http_archive stanza in the top level README.md to refer to the latest release."
-echo " 4. Update the version in the MODULE.bazel file"
+echo " 2. Update the http_archive stanza in the top level README.md to refer to the latest release."
 echo ""
 echo "!!! Hey, did you see the task list above, that is work you need to do !!!"
 read YESIDID
